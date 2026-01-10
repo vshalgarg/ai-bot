341aa11806b89560ee95b3caffb14a01d175da14
Vishal Garg Admin <vishalgarg@neepanlok.com>
2026-01-10 16:36:20 +0530

    corrected chroma port

--- src/main/java/in/codemonks/controller/ChatController.java
+++ src/main/java/in/codemonks/controller/ChatController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/chat")
+@RequestMapping("/api/chat")
 public class ChatController {
 
     private final ChatService chatService;
--- src/main/java/in/codemonks/controller/IngestController.java
+++ src/main/java/in/codemonks/controller/IngestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class IngestController {
 
     private final PdfService pdfService;
